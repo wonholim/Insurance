a51b1a4735c27a93860504259cd84fabe9c75a94
wonholim <kds0034@gmail.com>
2023-05-29 23:25:52 +0900

    Update Function

---
+++ src/main/java/domain/insurance/Accident.java
@@ -0,0 +1,81 @@
+package domain.insurance;
+
+public class Accident {
+    public Accident(){
+
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    private String phoneNum;
+    private String customerID;
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAccidentDate() {
+        return accidentDate;
+    }
+
+    public void setAccidentDate(String accidentDate) {
+        this.accidentDate = accidentDate;
+    }
+
+    public String getCarNum() {
+        return carNum;
+    }
+
+    public void setCarNum(String carNum) {
+        this.carNum = carNum;
+    }
+
+    public int getService() {
+        return service;
+    }
+
+    public void setService(int service) {
+        this.service = service;
+    }
+
+    private String customerName;
+    private String registrationNumber;
+    private String location;
+    private String accidentDate;
+    private String carNum;
+    private int service;
+}
---
+++ src/main/java/domain/insurance/Injury.java
@@ -0,0 +1,68 @@
+package domain.insurance;
+
+public class Injury {
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getInjuryDate() {
+        return InjuryDate;
+    }
+
+    public void setInjuryDate(String injuryDate) {
+        InjuryDate = injuryDate;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    private String customerID;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    private String customerName;
+    private String registrationNumber;
+    private String phoneNum;
+    private String location;
+    private String InjuryDate;
+    private String disease;
+}
